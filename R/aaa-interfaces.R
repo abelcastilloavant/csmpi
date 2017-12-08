4669e8c333862ff33bf2caca4b84ab92367e2c75
abelcastilloavant <abel.castillo@avant.com>
2017-12-08 14:55:18 -0600

    only one R file for both interface objects

--- R/aaa-interfaces.R
+++ R/aaa-interfaces.R
@@ -21,3 +21,22 @@ CloudInterface <- R6::R6Class("CloudInterface",
     exists_fn = ""
   )
 )
+
+DiskInterface <- R6::R6Class("DiskInterface",
+  public = list(
+    initialize = function(read_fn, write_fn) {
+      private$read_fn  <- read_fn
+      private$write_fn <- write_fn
+    },
+    read = function(tmpfile, params) {
+      private$read_fn(tmpfile, params)
+    },
+    write = function(obj, tmpfile, params) {
+      private$write_fn(obj, tmpfile, params)
+    }
+  ),
+  private = list(
+    read_fn = "",
+    write_fn = ""
+  )
+)
