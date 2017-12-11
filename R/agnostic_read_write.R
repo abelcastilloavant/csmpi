01e311dc1e07923e77b4cd162c7511682bb9f558
abelcastilloavant <abel.castillo@avant.com>
2017-12-11 14:12:11 -0600

    disk caching just piggybacks on existing logic

--- R/agnostic_read_write.R
+++ R/agnostic_read_write.R
@@ -1,6 +1,6 @@
 read_from_cloud_storage <- function(key, cloud_name, storage_format, params,
-                                      session_cache = getOption("csmpi.use_session_cache", TRUE),
-                                      disk_cache = getOption("csmpi.use_disk_cache", FALSE)) {
+                                      use_session_cache = getOption("csmpi.use_session_cache", TRUE),
+                                      use_disk_cache = getOption("csmpi.use_disk_cache", FALSE)) {
   if (isTRUE(use_session_cache) && isTRUE(use_disk_cache)) {
     stop("Both 'use_session_cache' and 'use_disk_cache' are TRUE - we currently do not allow this.")
   }
@@ -10,22 +10,19 @@ read_from_cloud_storage <- function(key, cloud_name, storage_format, params,
   }
 
   if (isTRUE(use_disk_cache) && `in_disk_cache?`(key, cloud_name, storage_format)) {
-    return(read_from_disk_cache(key, cloud_name, storage_format))
+    filename <- disk_cache_filename(key, cloud_name, storage_format)
+  } else {
+    filename <- tempfile(); on.exit(unlink(filename))
   }
 
   cloud_interface <- CLOUD_INTERFACES[[cloud_name]]
   disk_interface  <- DISK_INTERFACES[[storage_format]]
-  tmpfile         <- tempfile(); on.exit(unlink(tmpfile))
 
-  cloud_interface$get(key, tmpfile, params)
-  obj <- disk_interface$read(tmpfile, params)
+  cloud_interface$get(key, filename, params)
+  obj <- disk_interface$read(filename, params)
 
   if (isTRUE(use_session_cache)) {
     write_to_session_cache(obj, key, cloud_name, storage_format)
-  }
-
-  if (isTRUE(use_disk_cache)) {
-    write_to_disk_cache(obj, key, cloud_name, storage_format)
   }
   obj
 }
