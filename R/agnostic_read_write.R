1f2cb70f9349df6964f06a24f036a0a0de57126d
abelcastilloavant <abel.castillo@avant.com>
2017-12-11 14:35:29 -0600

    rm intermediate assignments, add disk caching to writing

--- R/agnostic_read_write.R
+++ R/agnostic_read_write.R
@@ -9,17 +9,16 @@ read_from_cloud_storage <- function(key, cloud_name, storage_format, params,
     return(read_from_session_cache(key, cloud_name, storage_format))
   }
 
-  if (isTRUE(use_disk_cache) && `in_disk_cache?`(key, cloud_name, storage_format)) {
+  if (isTRUE(use_disk_cache)) {
     filename <- disk_cache_filename(key, cloud_name, storage_format)
   } else {
     filename <- tempfile(); on.exit(unlink(filename))
   }
 
-  cloud_interface <- CLOUD_INTERFACES[[cloud_name]]
-  disk_interface  <- DISK_INTERFACES[[storage_format]]
-
-  cloud_interface$get(key, filename, params)
-  obj <- disk_interface$read(filename, params)
+  if (`fetch_from_cloud?`(use_disk_cache, key, cloud_name, storage_format)) {
+    CLOUD_INTERFACES[[cloud_name]]$get(key, filename, params)
+  }
+  obj <- DISK_INTERFACES[[storage_format]]$read(filename, params)
 
   if (isTRUE(use_session_cache)) {
     write_to_session_cache(obj, key, cloud_name, storage_format)
@@ -27,11 +26,13 @@ read_from_cloud_storage <- function(key, cloud_name, storage_format, params,
   obj
 }
 
-write_to_cloud_storage <- function(obj, key, cloud_name, storage_format, params) {
-  cloud_interface <- CLOUD_INTERFACES[[cloud_name]]
-  disk_interface  <- DISK_INTERFACES[[storage_format]]
-  tmpfile         <- tempfile(); on.exit(unlink(tmpfile))
+write_to_cloud_storage <- function(obj, key, cloud_name, storage_format, params, overwrite_disk_cache = FALSE) {
+  if (isTRUE(overwrite_disk_cache)) {
+    filename <- disk_cache_filename(key, cloud_name, storage_format)
+  } else {
+    filename <- tempfile(); on.exit(unlink(tmpfile))
+  }
 
-  disk_interface$write(obj, tmpfile, params)
-  cloud_interface$put(key, tmpfile, params)
+  DISK_INTERFACES[[storage_format]]$write(key, filename, params)
+  CLOUD_INTERFACES[[cloud_name]]$put(obj, filename, params)
 }
--- R/disk_cache.R
+++ R/disk_cache.R
@@ -7,3 +7,6 @@ disk_cache_filename <- function(key, cloud_name, storage_format) {
   file.exists(disk_cache_filename(key, cloud_name, storage_format))
 }
 
+`fetch_from_cloud?` <- function(use_disk_cache, key, cloud_name, storage_format) {
+  !(isTRUE(use_disk_cache) && `in_disk_cache?`(key, cloud_name, storage_format))
+}
