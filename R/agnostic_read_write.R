e3ac37ad5b7aa61184b8b6610a25ea3349c64e5c
abelcastilloavant <abel.castillo@avant.com>
2017-12-14 13:06:08 -0600

    added messaging

--- R/agnostic_read_write.R
+++ R/agnostic_read_write.R
@@ -6,6 +6,7 @@ read_from_cloud_storage <- function(key, cloud_name, storage_format, params,
   }
 
   if (isTRUE(use_session_cache) && `in_session_cache?`(key, cloud_name, storage_format)) {
+    message("reading ", key, " from session cache")
     return(read_from_session_cache(key, cloud_name, storage_format))
   }
 
@@ -16,11 +17,14 @@ read_from_cloud_storage <- function(key, cloud_name, storage_format, params,
   }
 
   if (`fetch_from_cloud?`(use_disk_cache, key, cloud_name, storage_format)) {
+    message("reading ", key, " from ", cloud_name)
     CLOUD_INTERFACES[[cloud_name]]$get(key, filename, params)
   }
+
   obj <- DISK_INTERFACES[[storage_format]]$read(filename, params)
 
   if (isTRUE(use_session_cache)) {
+    message("writing ", key, " to session cache")
     write_to_session_cache(obj, key, cloud_name, storage_format)
   }
   obj
