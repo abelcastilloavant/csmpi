8d3c9a6c6121dd59947577a9c4984600491aacfa
abelcastilloavant <abel.castillo@avant.com>
2017-12-18 16:40:53 -0600

    typofix

--- R/session_cache.R
+++ R/session_cache.R
@@ -4,7 +4,7 @@ get_session_cache_key <- function(key, cloud_name, storage_format) {
   digest::digest(list(key = key, cloud_name = cloud_name, storage_format = storage_format))
 }
 
-`in_session_cache?` <- function(key, session_cache_key) {
+`in_session_cache?` <- function(session_cache_key) {
   session_cache$exists(session_cache_key)
 }
 
