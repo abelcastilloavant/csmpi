146709a65d8eed6ada094a84f605274b06b37803
abelcastilloavant <abel.castillo@avant.com>
2017-12-18 15:49:29 -0600

    simpler cache helper functions

--- R/disk_cache.R
+++ R/disk_cache.R
@@ -1,12 +1,8 @@
-disk_cache_filename <- function(key, cloud_name, storage_format) {
+get_disk_cache_filename <- function(key, cloud_name, storage_format) {
   file.path(getOption("csmpi.disk_cache_dir", "~/.csmpicache"), cloud_name, storage_format,
     digest::digest(key))
 }
 
-`in_disk_cache?` <- function(key, cloud_name, storage_format) {
-  file.exists(disk_cache_filename(key, cloud_name, storage_format))
-}
-
-`fetch_from_cloud?` <- function(use_disk_cache, key, cloud_name, storage_format) {
-  !(isTRUE(use_disk_cache) && `in_disk_cache?`(key, cloud_name, storage_format))
+`fetch_from_cloud?` <- function(use_disk_cache, filename) {
+  !(isTRUE(use_disk_cache) && file.exists(filename))
 }
